294dfe191f9a3cf45f02f135cf7cd4b94228d9f8
Younes Mel <103428027+Youn35-Mel@users.noreply.github.com>
2022-12-21 10:44:41 +0000

    header footer setup

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Login.scss";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="auth">
+      <h1>Register</h1>
+      <form>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          name="username"
+          // onChange={handleChange}
+        />
+        <input
+          required
+          type="email"
+          placeholder="email"
+          name="email"
+          // onChange={handleChange}
+        />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          name="password"
+          // onChange={handleChange}
+        />
+        {/* <button onClick={handleSubmit}>Register</button> */}
+        {/* {err && <p>{err}</p>} */}
+        <span>
+          Do you have an account? <Link to="/login">Login</Link>
+        </span>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
