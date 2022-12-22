92df62d99f25776cffad3d181cb8825508799ca8
Younes Mel <103428027+Youn35-Mel@users.noreply.github.com>
2022-12-22 14:57:14 +0000

    single page

--- client/src/pages/Single.js
+++ client/src/pages/Single.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, Link } from "react-router-dom";
 import { AuthContext } from "../context/authContext";
+import moment from "moment";
+import Edit from "../img/edit.png";
+import Delete from "../img/delete.png";
+import DOMPurify from "dompurify";
+import Menu from "../components/Menu/Menu";
 
 const Single = () => {
   const [post, setPost] = useState({});
@@ -42,32 +47,29 @@ const Single = () => {
   return (
     <div className="single">
       <div className="content">
-        {/* <img src={`../upload/${post?.img}`} alt="" /> */}
+        <img src={`../upload/${post?.img}`} alt="" />
         <div className="user">
-          {/* {post.userImg && <img */}
-          {/* src={post.userImg} */}
-          {/* alt="" */}
-          {/* />} */}
+          {post.userImg && <img src={post.userImg} alt="" />}
           <div className="info">
-            {/* <span>{post.username}</span> */}
-            {/* <p>Posted {moment(post.date).fromNow()}</p> */}
+            <span>{post.username}</span>
+            <p>Posted {moment(post.date).fromNow()}</p>
           </div>
-          {/* {currentUser.username === post.username && ( */}
-          <div className="edit">
-            {/* <Link to={`/write?edit=2`} state={post}> */}
-            {/* <img src={Edit} alt="" /> */}
-            {/* </Link> */}
-            {/* <img onClick={handleDelete} src={Delete} alt="" /> */}
-          </div>
-          {/* )} */}
+          {currentUser.username === post.username && (
+            <div className="edit">
+              <Link to={`/write?edit=2`} state={post}>
+                <img src={Edit} alt="" />
+              </Link>
+              <img onClick={handleDelete} src={Delete} alt="" />
+            </div>
+          )}
         </div>
-        {/* <h1>{post.title}</h1> */}
-        {/* <p */}
-        {/* dangerouslySetInnerHTML={{ */}
-        {/* __html: DOMPurify.sanitize(post.desc), */}
-        {/* }}></p>{" "} */}
+        <h1>{post.title}</h1>
+        <p
+          dangerouslySetInnerHTML={{
+            __html: DOMPurify.sanitize(post.desc),
+          }}></p>{" "}
       </div>
-      {/* <Menu cat={post.cat} /> */}
+      <Menu cat={post.cat} />
     </div>
   );
 };
